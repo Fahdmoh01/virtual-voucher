0fc48f55808749ecb2d4502a4b08e8ca1589e592
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-08-18 17:36:44 +0100

    added view for users to register as participant

--- core/api/views/events.py
+++ core/api/views/events.py
@@ -6,74 +6,124 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import Event
+from api.models import Event,User
 from api.serializers import EventsSerializer
 
 
 class EventListAPI(APIView):
-    '''For getting all events created by the requester'''
-    permission_classes = (permissions.IsAuthenticated,)
+	'''For getting all events created by the requester'''
+	permission_classes = (permissions.IsAuthenticated,)
 
-    def get(self, request, *args, **kwargs):
-        '''Uses get request to fetch all events created by the user'''
-        user = request.user
-        events = Event.objects.filter(created_by=user).order_by("-id")
-        serializer = EventsSerializer(events, many=True)
-        return Response({
-            "events": serializer.data,
-        }, status=status.HTTP_200_OK)
+	def get(self, request, *args, **kwargs):
+		'''Uses get request to fetch all events created by the user'''
+		user = request.user
+		events = Event.objects.filter(created_by=user).order_by("-id")
+		serializer = EventsSerializer(events, many=True)
+		return Response({
+			"events": serializer.data,
+		}, status=status.HTTP_200_OK)
 
 
 class CUDEventAPI(APIView):
-    '''For Create, Update, Delete of events'''
-    permission_classes = (permissions.IsAuthenticated,)
+	'''For Create, Update, Delete of events'''
+	permission_classes = (permissions.IsAuthenticated,)
 
-    def post(self, request, *args, **kwargs):
-        '''Uses the post request to create a new event'''
-        user = request.user
-        name = request.data.get("name")
-        date = request.data.get("date")
-        event = Event.objects.create(
-            name=name,
-            date=date,
-            created_by=user
-        )
-        serializer = EventsSerializer(event, many=False)
-        return Response({
-            "event": serializer.data
-        }, status=status.HTTP_201_CREATED)
+	def post(self, request, *args, **kwargs):
+		'''Uses the post request to create a new event'''
+		user = request.user
+		name = request.data.get("name")
+		date = request.data.get("date")
+		event = Event.objects.create(
+			name=name,
+			date=date,
+			created_by=user
+		)
+		serializer = EventsSerializer(event, many=False)
+		return Response({
+			"event": serializer.data
+		}, status=status.HTTP_201_CREATED)
 
-    def put(self, request, *args, **kwargs):
-        '''Uses the put request to update existing event'''
-        user = request.user
-        event_id = request.data.get("event_id")
-        event = Event.objects.filter(id=event_id, created_by=user).first()  # noqa
-        name = request.data.get("name")
-        date = request.data.get("date")
-        if event is not None:
-            event.name = name
-            event.date = date
-            event.save()
-            serializer = EventsSerializer(event, many=False)
-            return Response({
-                "event": serializer.data
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                "message": "Event Not Found"
-            }, status=status.HTTP_404_NOT_FOUND)
+	def put(self, request, *args, **kwargs):
+		'''Uses the put request to update existing event'''
+		user = request.user
+		event_id = request.data.get("event_id")
+		event = Event.objects.filter(id=event_id, created_by=user).first()  # noqa
+		name = request.data.get("name")
+		date = request.data.get("date")
+		if event is not None:
+			event.name = name
+			event.date = date
+			event.save()
+			serializer = EventsSerializer(event, many=False)
+			return Response({
+				"event": serializer.data
+			}, status=status.HTTP_200_OK)
+		else:
+			return Response({
+				"message": "Event Not Found"
+			}, status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request, *args, **kwargs):
-        '''Uses the delete request to delete an event'''
-        user = request.user
-        event_id = request.data.get("event_id")
-        event = Event.objects.filter(id=event_id, created_by=user).first()  # noqa
-        if event is not None:
-            event.delete()
-            return Response({
-                "message": "Event Deleted Successfully",
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                "message": "Event Not Found"
-            }, status=status.HTTP_404_NOT_FOUND)
+	def delete(self, request, *args, **kwargs):
+		'''Uses the delete request to delete an event'''
+		user = request.user
+		event_id = request.data.get("event_id")
+		event = Event.objects.filter(id=event_id, created_by=user).first()  # noqa
+		if event is not None:
+			event.delete()
+			return Response({
+				"message": "Event Deleted Successfully",
+			}, status=status.HTTP_200_OK)
+		else:
+			return Response({
+				"message": "Event Not Found"
+			}, status=status.HTTP_404_NOT_FOUND)
+		
+
+
+class AddEventParticipantAPI(APIView):
+	'''For users to register as participants of the event'''
+	
+	permission_classes = [permissions.IsAuthenticated]
+	
+	def post(self, request, *args, **kwargs):
+		'''Uses the post request to add a participant to the event'''
+		user = request.user
+		event_id = request.data.get("event_id")
+		event = Event.objects.filter(id=event_id).first()
+		if event is not None:
+			event.participants.add(user)
+			event.save()
+			serializer = EventsSerializer(event, many=False)
+			return Response({
+				"event": serializer.data
+			}, status=status.HTTP_200_OK)
+		else:
+			return Response({
+				"message": "Event Not Found"
+			}, status=status.HTTP_200_OK)
+		
+
+
+class RemoveParticipant(APIView):
+	'''Used by event organizers to remove participants from events'''
+	permission_classes = [permissions.IsAuthenticated]
+
+	def delete(self, request, *args, **kwargs):
+		'''Uses delete request to remove participants from event part'''
+		event_id = request.data.get("event_id")
+		event = Event.objects.filter(id=event_id).first()
+
+		if not Event:
+			return Response({"message": "Event Not Found"}, status=status.HTTP_404_NOT_FOUND)
+		
+		participant_email = request.data.get("participant_email")
+		participant = User.objects.filter(email=participant_email).first()
+
+		#Check if the participant is in the event participants
+		if participant in event.participants.all():
+			event.participants.remove(participant)
+			return Response({"message": "Participant Removed"},status=status.HTTP_200_OK)
+		else:
+			return Response({
+				"message": "User is Not a Participant of the Event"
+			},status=status.HTTP_200_OK)
